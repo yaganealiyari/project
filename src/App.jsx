aa08117c34d192ca672ac9fb2de2fb83037fa758
yaganealiyari <yna.aliyari@gmail.com>
2026-08-06 09:15:34 +0330

    start

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Prouducts from "./Components/Prouducts";
+import Prouductcard from "./Components/Prouductcard";
+
+export default function App() {
+  return (
+    <div>
+      <Prouducts />
+      <Prouductcard />
+    </div>
+  );
+}
